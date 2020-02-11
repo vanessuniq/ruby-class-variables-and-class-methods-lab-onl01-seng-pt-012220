5a6427e9b4638ddf8a6174bfffd847c203d6d917
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-11 06:46:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,8 @@ class Song
       genre_count = {}
       @@genres.each do |genre| 
         if genre_count[genre].nil?
-          
+          genre_count[genre] = 0
+          genre_count[genre] += 1
         end 
       
       end
