2f9f300b258200cbead6ed78658a1bf3598bd1b1
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-11 06:44:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
     def self.genre_count
       genre_count = {}
       @@genres.each do |genre| 
-        (genre_count[genre]).to_i += 1
+        genre_count[genre] += 1
       end
       genre_count
     end
